556e18e8e53ddfe6cc733ffe4c02f942a5e207c6
kenjuleong <113619836+kenjuleong@users.noreply.github.com>
2023-03-03 02:11:13 +0800

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,24 @@
-import puppeteer from 'puppeteer';
+import {executablePath} from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker';
+import { DEFAULT_INTERCEPT_RESOLUTION_PRIORITY } from 'puppeteer';
+
 import fs from 'fs';
 
 const urlList = ['https://weirdkaya.com'];
 const wait = (time) => new Promise((resolve) => { setTimeout(() => resolve(), time) });
 
+puppeteer.use(
+  AdblockerPlugin({
+    interceptResolutionPriority: DEFAULT_INTERCEPT_RESOLUTION_PRIORITY
+  })
+)
+
 urlList.forEach(async (url) => {
   try {
-      const browser = await puppeteer.launch();
+      const browser = await puppeteer.launch({
+          executablePath: executablePath()
+      });
       const page = await browser.newPage();
       await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.0 Safari/537.36");
       await page.setViewport({
