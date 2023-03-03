0215b1d259aa6fdc6f126ba8499676b9831cbb42
kenjuleong <113619836+kenjuleong@users.noreply.github.com>
2023-03-04 00:54:34 +0800

    Update web.js

--- web.js
+++ web.js
@@ -44,7 +44,7 @@ urlList.forEach(async (url) => {
       openedURLs.add(hrefs[i]);
       try {
         await currentPage.goto(hrefs[i])//{waitUntil: ['networkidle0'], timeout:0});
-        await wait(60000);
+        await wait(120000);
         const pageURL = (await currentPage.url()).slice(8).split("/").join("@");
         await currentPage.screenshot({path: `./${url.slice(8)}/${pageURL == url ? "index" : pageURL}.png`, fullPage: true});
         console.log(`
